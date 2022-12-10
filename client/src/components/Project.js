3e4ff3ac0d5a5fed1783836a2a5820ea3e05fb19
Johnny <johnathan.mnn@gmail.com>
2022-12-10 17:33:29 -0600

    Projects can now be deleted

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,12 +1,35 @@
 import React from "react";
 
+async function deleteProject(project_Id) {
+  console.log(project_Id)
+  const click = true;
+  // Make post to database so we can show it on the site
+  if (click) {
+    const response = await fetch(`/api/projects/${project_Id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      console.log('Post Deleted');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  };
+
 export default function Project({thisProject}) {
+  const projectId = thisProject._id;
 return (
         <div key={thisProject._id} className="project">
       <h3>
         {thisProject.title}
       </h3>
       <code>{thisProject.description}</code> <br />
+      <button onClick={() => deleteProject(projectId)} >
+      <span id="projectId">{thisProject._id}</span>
+      </button>
       </div>
 )
 }
