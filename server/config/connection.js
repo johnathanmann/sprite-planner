c6f615e3639840967ee6cf1d688e477ae22d0483
Johnny <johnathan.mnn@gmail.com>
2022-12-10 20:06:58 -0600

    Updated node

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/sprite-planner',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1/sprite-planner',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
